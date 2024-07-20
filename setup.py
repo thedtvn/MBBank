7b0fa51477aa7bd7e67a67f347e435283c5d19ab
thedtvn <duongtuan30306@gmail.com>
2024-07-20 20:51:36 +0700

    fix python packages setup err

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
    long_description_content_type="text/markdown",
    url='https://github.com/thedtvn/MBBank',
    author='The DT',
-   packages=["mbbank"],
+   packages=["mbbank", "mbbank.wasm_helper"],
    install_requires=req,
    include_package_data=True
 )
